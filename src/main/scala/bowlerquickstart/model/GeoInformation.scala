88b34f9afc6b195d922d1bd0e9e92f43e5f77114
Brian Chen <brianchen8990@gmail.com>
2012-04-09 14:32:48 +0800

    add geoinformation store function and change something in geoinfos table

--- src/main/scala/bowlerquickstart/model/GeoInformation.scala
+++ src/main/scala/bowlerquickstart/model/GeoInformation.scala
@@ -1,3 +1,51 @@
 package bowlerquickstart.model
 
-case class GeoInformation(val latitude:String, val longitude:String, val bindUser:String, val time:String)
+case class GeoInformation(val latitude:String, val longitude:String, val bindUser:String, val date:String, val time:String, val geoType:Int)
+
+trait GeoType{
+  def id : Int
+  def toString : String
+}
+
+object Start extends GeoType{
+  override def id = 1
+  override def toString = "start"
+}
+
+object Process extends GeoType{
+  override def id = 2
+  override def toString = "process"
+}
+
+object End extends GeoType{
+  override def id = 3
+  override def toString = "end"
+}
+
+object Single extends GeoType{
+  override def id = 4
+  override def toString = "single"
+}
+
+object GeoTypes{
+  val start = Start
+  val process = Process
+  val end = End
+  val single = Single
+  
+  val list = List(start,process,end)
+  
+  def getTypeById(id:Int) = list.filter(gt => gt.id == id).first
+  
+  def getTypeByString(strType:String) = {
+    if(strType == "start"){
+      GeoTypes.start
+    }else if(strType == "process"){
+      GeoTypes.process
+    }else if(strType == "end"){
+      GeoTypes.end
+    }else{
+      GeoTypes.single
+    }
+  }
+}
--- src/main/scala/bowlerquickstart/service/DBTableStore.scala
+++ src/main/scala/bowlerquickstart/service/DBTableStore.scala
@@ -8,6 +8,13 @@ import java.util.Date
 import bowlerquickstart.UserStore
 import bowlerquickstart.model.User
 import org.joda.time.DateTime
+import bowlerquickstart.GeoInformationStore
+import bowlerquickstart.model.GeoInformation
+import bowlerquickstart.model.GeoInformation
+import bowlerquickstart.model.GeoTypes
+import java.text.SimpleDateFormat
+import org.joda.time.format.DateTimeFormatter
+import org.joda.time.format.DateTimeFormat
 
 class DBTableStore {
   import Tables._
@@ -15,8 +22,18 @@ class DBTableStore {
 
 class DBUserStore extends UserStore{
   import Tables._
-  implicit def db2user(user:DBUser) = new User(user.userName,user.password,user.role,user.status)
-  implicit def user2db(user:User) = new DBUser(0,user.userName,user.password,user.role,new Timestamp(new DateTime().getMillis()),true)
+  implicit def db2user(user:DBUser) = 
+    new User(user.userName,
+    		user.password,
+    		user.role,
+    		user.status)
+  implicit def user2db(user:User) = 
+    new DBUser(0,
+    		user.userName,
+    		user.password,
+    		user.role,
+    		new Timestamp(new DateTime().getMillis()),
+    		true)
   
   def addUser(user:User) : User = {
     inTransaction{
@@ -58,6 +75,56 @@ class DBUserStore extends UserStore{
   }
 }
 
+class DBGeoInformationStore extends GeoInformationStore{
+  import Tables._
+  implicit def db2geoinfo(geoinfo:DBGeoInformation) =
+    new GeoInformation(geoinfo.latitude,
+    				geoinfo.longitude,
+    				geoinfo.bindUser,
+    				new SimpleDateFormat("yyyy-MM-dd").format(geoinfo.created),
+    				new SimpleDateFormat("HH:mm").format(geoinfo.created),
+    				GeoTypes.getTypeByString(geoinfo.geoType).id)
+  implicit def geoinfo2db(geoinfo:GeoInformation) = 
+    new DBGeoInformation(0,
+    				geoinfo.latitude,
+    				geoinfo.longitude,
+    				geoinfo.bindUser,
+    				new Timestamp(new DateTime().getMillis()),
+    				GeoTypes.getTypeById(geoinfo.geoType).toString())
+  
+  def addGeoInfo(geoinfo:GeoInformation) : GeoInformation ={
+    inTransaction{
+      val newGeoInfo = Tables.geoinfos.insert(geoinfo2db(geoinfo))
+      db2geoinfo(newGeoInfo)
+    }
+  }
+  
+  def updateGeoInfo(geoinfo:GeoInformation) : GeoInformation ={
+    val dateString:String = geoinfo.date + " " + geoinfo.time
+    val formatter:DateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")
+    val newDate:DateTime = formatter.parseDateTime(dateString)
+    inTransaction{
+      Tables.geoinfos.update(g => 
+        					where(g.bindUser === geoinfo.bindUser and g.geoType === GeoTypes.getTypeById(4).toString())
+    		  				set(g.latitude := geoinfo.latitude,
+    		  				    g.longitude := geoinfo.longitude,
+    		  				    g.created := new Timestamp(newDate.getMillis()))
+    		  				    )
+    }
+    geoinfo
+  }
+  
+  def checkGeoInfo(bindUser:String) : GeoInformation ={
+    inTransaction{
+      val geoinfo = from(geoinfos)(g => 
+        						where(g.bindUser === bindUser and g.geoType === GeoTypes.getTypeById(4).toString())
+        						select(g)
+        						orderBy(g.created desc)).first
+      db2geoinfo(geoinfo)
+    }
+  }
+}
+
 case class DBUser(
     val id:Long, val userName:String, 
     val password:String, val role:String,
@@ -68,8 +135,8 @@ case class DBUser(
 case class DBGeoInformation(
     val id:Long, val latitude:String,
     val longitude:String, val bindUser:String,
-    val created:Timestamp) extends KeyedEntity[Long]{
-  def this() = this(0,"","","",new Timestamp(new Date().getTime()))
+    val created:Timestamp, val geoType:String) extends KeyedEntity[Long]{
+  def this() = this(0,"","","",new Timestamp(new Date().getTime()),"")
 }
 
 case class DBSocialNetwork(

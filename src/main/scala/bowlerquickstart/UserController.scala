84f446b1f60c1c1837f071a164f7a1e8bad53481
Brian Chen <brianchen8990@gmail.com>
2012-06-10 20:33:40 +0800

    add user register function for app on server

--- src/main/scala/bowlerquickstart/UserController.scala
+++ src/main/scala/bowlerquickstart/UserController.scala
@@ -4,6 +4,7 @@ import org.bowlerframework.model.ParameterMapper
 import org.bowlerframework.model.Validations
 import org.bowlerframework.view.Renderable
 import org.bowlerframework.controller.FunctionNameConventionRoutes
+import bowlerquickstart.model.User
 
 class UserController(userStore:UserStore) extends SquerylController with ParameterMapper with Validations with Renderable  with FunctionNameConventionRoutes{
 	def `GET /user/login`(username:String,password:String) = {
@@ -11,4 +12,8 @@ class UserController(userStore:UserStore) extends SquerylController with Paramet
 	  render(result)
 	}
 	
+	def `GET /user/new`(username:String,password:String,role:String) = {
+	  val newUser = userStore.addUser(new User(username,password,role,"true"))
+	  render(newUser)
+	}
 }
